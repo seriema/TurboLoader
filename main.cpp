e4977161108d934fb4ce7b7a4b40f83ac5a02260
John-Philip Johansson <seriema@gmail.com>
2016-04-07 22:17:04 +0200

    Create 3D shader example

--- main.cpp
+++ main.cpp
@@ -1,239 +1,105 @@
-#include <SDL2/SDL.h>
-//#include <SDL2/SDL_opengl.h>
-#include <SDL2/SDL_opengles2.h>
+// headerphile.com - main.cpp
+// This is the main manager the class that controls everything
+// It doesn't do much on it's own, it just tells the other classes do to stuff
+//
+// Compile : clang++ main.cpp EventHandler.cpp -o Part_04 -lGL -lSDL2 -lGLEW -lstd=c++11
+//
 
-//Main loop flag
-bool quit = false;
+#include <iostream>
 
+#include "Renderer.h"
+#include "EventHandler.h"
+#include "Math.h"
 
-//Starts up SDL, creates window, and initializes OpenGL
-bool init();
+// Handles all our SDL2 event ( quit, keydown, ++ )
+EventHandler handler;
 
-//Initializes matrices and clear color
-bool initGL();
+// Handles everything related to OpenGL
+Renderer renderer;
 
-//Input handler
-void handleKeys( unsigned char key, int x, int y );
+// Handles a single model with its vertexes and matrix
+Model model;
 
-//Per frame update
-void update();
+void PrintCommands();
+void UpdateModel();
 
-//Renders quad to the screen
-void render();
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//OpenGL context
-SDL_GLContext gContext;
-
-//Render flag
-bool gRenderQuad = true;
-
-void _SDL_GL_SetAttribute(SDL_GLattr attr, int value)
+void Render()
 {
-  if (SDL_GL_SetAttribute(attr, value) != 0)
-  {
-    printf("SDL_GL_SetAttribute(%d, %d) failed: %s\n", attr, value, SDL_GetError());
-  }
+	// Tell Renderer to clear everything
+	renderer.RenderStart();
+
+	// Set the matrix in the model
+	// This has to be done once per model
+	renderer.SetMatrix( model.GetModel() );
+
+	// Render our model
+	renderer.RenderModel(model);
+
+	// Tell our Renderer to swap the buffers
+	renderer.RenderEnd();
 }
 
-bool init()
+void Update()
 {
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	while ( !handler.WasQuit() )
 	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		return false;
-	}
+		// Update our event handler
+		handler.Update();
 
-	//Use OpenGL ES
-	_SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 2 );
-	_SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 0 );
+		// Update our model position
+		UpdateModel();
 
-	//hide mouse cursor early
-//	SDL_ShowCursor(0);
-
-	_SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-	_SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-	_SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-	_SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
-	_SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-//	_SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-
-	SDL_DisplayMode dispMode;
-	//SDL_GetDesktopDisplayMode(0, &dispMode);
-	SDL_GetCurrentDisplayMode(0, &dispMode);
-
-	printf("Screen size: %d %d\n", dispMode.w, dispMode.h);
-	//Create window
-	gWindow = SDL_CreateWindow(
-		"SDL Tutorial",
-		//SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		dispMode.w, dispMode.h,
-//		640, 480,
-//		0, 0,
-		SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_SHOWN
-	);
-	if( gWindow == NULL )
-	{
-		printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-		return false;
-	}
-
-	//Create context
-	gContext = SDL_GL_CreateContext( gWindow );
-	if( gContext == NULL )
-	{
-		printf( "OpenGL context could not be created! SDL Error: %s\n", SDL_GetError() );
-		return false;
-	}
-	SDL_GL_MakeCurrent(gWindow, gContext);
-
-	//Use Vsync
-	if( SDL_GL_SetSwapInterval( 1 ) < 0 )
-	{
-		printf( "Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError() );
-	}
-
-//	glViewport(0, 0, 640, 480);
-//	glOrtho(0, 640, 480, 0, -1.0, 1.0);
-
-	//Initialize OpenGL
-	if( !initGL() )
-	{
-		printf( "Unable to initialize OpenGL!\n" );
-		return false;
-	}
-
-	return true;
-}
-
-bool initGL()
-{
-	//Initialize Projection Matrix
-//	glMatrixMode( GL_PROJECTION );
-//	glLoadIdentity();
-
-	//Check for error
-	GLenum error = glGetError();
-	if( error != GL_NO_ERROR )
-	{
-		return false;
-	}
-
-	//Initialize Modelview Matrix
-//	glMatrixMode( GL_MODELVIEW );
-//	glLoadIdentity();
-
-	//Check for error
-	error = glGetError();
-	if( error != GL_NO_ERROR )
-	{
-		return false;
-	}
-
-	//Initialize clear color
-	glClearColor( 0.5f, 0.f, 0.f, 1.f );
-
-	//Check for error
-	error = glGetError();
-	if( error != GL_NO_ERROR )
-	{
-		return false;
-	}
-
-	return true;
-}
-
-void handleKeys( unsigned char key, int x, int y )
-{
-	if( key == 'q' )
-	{
-		quit = true;
+		Render();
 	}
 }
 
-void update()
+void UpdateModel()
 {
-	//No per frame update needed
-}
-
-void render()
-{
-	//Clear color buffer
-	glClearColor(1.f, 0.f, 1.f, 1.f);
-	glClear(GL_COLOR_BUFFER_BIT);
-}
-
-void close()
-{
-	//Destroy window
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-
-	//Quit SDL subsystems
-	SDL_Quit();
-}
-
-int main( int argc, char* args[] )
-{
-	//Start up SDL and create window
-	printf("Initializing...\n");
-	if( !init() )
+	if (handler.IsKeyDown(SDLK_r))
 	{
-		printf( "Failed to initialize!\n" );
+		model.ResetMatrix();
 	}
 	else
 	{
-		printf("Initialized\n");
+		glm::vec3 axis = Math::GetAxis(handler);
 
-		//Event handler
-		SDL_Event e;
-
-		//Enable text input
-		SDL_StartTextInput();
-
-		printf("Entering main loop\n");
-		//While application is running
-		while( !quit )
-		{
-	//		printf("Looping\n");
-
-			//Handle events on queue
-			while( SDL_PollEvent( &e ) != 0 )
-			{
-				//User requests quit
-				if( e.type == SDL_QUIT )
-				{
-					quit = true;
-				}
-					//Handle keypress with current mouse position
-				else if( e.type == SDL_TEXTINPUT )
-				{
-					int x = 0, y = 0;
-					SDL_GetMouseState( &x, &y );
-					handleKeys( e.text.text[ 0 ], x, y );
-				}
-			}
-
-			//Render quad
-			render();
-
-			//Update screen
-			SDL_GL_SwapWindow( gWindow );
-		}
-
-		//Disable text input
-		SDL_StopTextInput();
+		model.Translate(axis);
 	}
+}
 
-	//Free resources and close SDL
-	close();
+int main(int argc, char *argv[])
+{
+	if ( !renderer.Init() )
+		return -1;
+
+	PrintCommands();
+
+	// Set up our only shader in Shader
+	renderer.SetUpShader("vert.glsl", "frag.glsl");
+
+	// Create the Model
+	if (!model.SetupBufferObjects())
+		return -1;
+
+	// Set the shader 
+	renderer.SetShader(model);
+
+	// Run our main update loop
+	Update();
+
+	renderer.Cleanup();
 
 	return 0;
+}
+
+void PrintCommands()
+{
+	std::cout << "Controls :"
+		<< "\n\tMove left : \t\t\t Left"
+		<< "\n\tMove right : \t\t\t Right"
+		<< "\n\tMove up : \t\t\t Up"
+		<< "\n\tMove down : \t\t\t Down"
+		<< "\n\tMove clockwise : \t\t w"
+		<< "\n\tMove counter-clockwise : \t s"
+		<< std::endl;
 }
