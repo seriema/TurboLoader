89c91ba2b9eac4a367521447029e0c3d244ee078
John-Philip Johansson <seriema@gmail.com>
2016-04-24 13:56:21 +0200

    Move dependency setup to its own CMakeLists.txt
    To separate the platform logic in our CMake files the 3rd party dependency files
    and logic have all been moved into the dependencies/ folder. I tried creating a
    library output file that could be easily included in the root CMakeLists.txt but
    it was a lot more difficult than expected. Especially on Travis-CI where I gave
    up because of linker order build errors that wouldn't happen on any other
    platform (I gave up here: http://stackoverflow.com/a/19905704).
    
    * Add a CMakeLists.txt to encapsulate all dependency logic
    ** Expose dependency directories through:
    *** DEPENDENCIES_INCLUDES
    *** DEPENDENCIES_SOURCES
    *** DEPENDENCIES_LIBRARIES
    * Move Find*.cmake files into dependencies/ folder

--- dependencies/cmake/FindSDL2.cmake
+++ dependencies/cmake/FindSDL2.cmake
@@ -66,44 +66,44 @@
 #  License text for the above reference.)
 
 SET(SDL2_SEARCH_PATHS
-        ~/Library/Frameworks
-        /Library/Frameworks
-        /usr/local
-        /usr
-        /sw # Fink
-        /opt/local # DarwinPorts
-        /opt/csw # Blastwave
-        /opt
-        )
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local
+	/usr
+	/sw # Fink
+	/opt/local # DarwinPorts
+	/opt/csw # Blastwave
+	/opt
+	)
 
 FIND_PATH(SDL2_INCLUDE_DIR SDL.h
-        HINTS
-        $ENV{SDL2DIR}
-        PATH_SUFFIXES include/SDL2 include
-        PATHS ${SDL2_SEARCH_PATHS}
-        )
+	HINTS
+	$ENV{SDL2DIR}
+	PATH_SUFFIXES include/SDL2 include
+	PATHS ${SDL2_SEARCH_PATHS}
+	)
 
 FIND_LIBRARY(SDL2_LIBRARY_TEMP
-        NAMES SDL2
-        HINTS
-        $ENV{SDL2DIR}
-        PATH_SUFFIXES lib64 lib
-        PATHS ${SDL2_SEARCH_PATHS}
-        )
+	NAMES SDL2
+	HINTS
+	$ENV{SDL2DIR}
+	PATH_SUFFIXES lib64 lib
+	PATHS ${SDL2_SEARCH_PATHS}
+	)
 
 IF(NOT SDL2_BUILDING_LIBRARY)
     IF(NOT ${SDL2_INCLUDE_DIR} MATCHES ".framework")
-        # Non-OS X framework versions expect you to also dynamically link to
-        # SDL2main. This is mainly for Windows and OS X. Other (Unix) platforms
-        # seem to provide SDL2main for compatibility even though they don't
-        # necessarily need it.
-        FIND_LIBRARY(SDL2MAIN_LIBRARY
-                NAMES SDL2main
-                HINTS
-                $ENV{SDL2DIR}
-                PATH_SUFFIXES lib64 lib
-                PATHS ${SDL2_SEARCH_PATHS}
-                )
+	# Non-OS X framework versions expect you to also dynamically link to
+	# SDL2main. This is mainly for Windows and OS X. Other (Unix) platforms
+	# seem to provide SDL2main for compatibility even though they don't
+	# necessarily need it.
+	FIND_LIBRARY(SDL2MAIN_LIBRARY
+		NAMES SDL2main
+		HINTS
+		$ENV{SDL2DIR}
+		PATH_SUFFIXES lib64 lib
+		PATHS ${SDL2_SEARCH_PATHS}
+		)
     ENDIF(NOT ${SDL2_INCLUDE_DIR} MATCHES ".framework")
 ENDIF(NOT SDL2_BUILDING_LIBRARY)
 
@@ -125,9 +125,9 @@ ENDIF(MINGW)
 IF(SDL2_LIBRARY_TEMP)
     # For SDL2main
     IF(NOT SDL2_BUILDING_LIBRARY)
-        IF(SDL2MAIN_LIBRARY)
-            SET(SDL2_LIBRARY_TEMP ${SDL2MAIN_LIBRARY} ${SDL2_LIBRARY_TEMP})
-        ENDIF(SDL2MAIN_LIBRARY)
+	IF(SDL2MAIN_LIBRARY)
+	    SET(SDL2_LIBRARY_TEMP ${SDL2MAIN_LIBRARY} ${SDL2_LIBRARY_TEMP})
+	ENDIF(SDL2MAIN_LIBRARY)
     ENDIF(NOT SDL2_BUILDING_LIBRARY)
 
     # For OS X, SDL2 uses Cocoa as a backend so it must link to Cocoa.
@@ -137,19 +137,19 @@ IF(SDL2_LIBRARY_TEMP)
     # So I use a temporary variable until the end so I can set the
     # "real" variable in one-shot.
     IF(APPLE)
-        SET(SDL2_LIBRARY_TEMP ${SDL2_LIBRARY_TEMP} "-framework Cocoa")
+	SET(SDL2_LIBRARY_TEMP ${SDL2_LIBRARY_TEMP} "-framework Cocoa")
     ENDIF(APPLE)
 
     # For threads, as mentioned Apple doesn't need this.
     # In fact, there seems to be a problem if I used the Threads package
     # and try using this line, so I'm just skipping it entirely for OS X.
     IF(NOT APPLE)
-        SET(SDL2_LIBRARY_TEMP ${SDL2_LIBRARY_TEMP} ${CMAKE_THREAD_LIBS_INIT})
+	SET(SDL2_LIBRARY_TEMP ${SDL2_LIBRARY_TEMP} ${CMAKE_THREAD_LIBS_INIT})
     ENDIF(NOT APPLE)
 
     # For MinGW library
     IF(MINGW)
-        SET(SDL2_LIBRARY_TEMP ${MINGW32_LIBRARY} ${SDL2_LIBRARY_TEMP})
+	SET(SDL2_LIBRARY_TEMP ${MINGW32_LIBRARY} ${SDL2_LIBRARY_TEMP})
     ENDIF(MINGW)
 
     # Set the final string here so the GUI reflects the final state.
